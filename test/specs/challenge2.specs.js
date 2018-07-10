21f38aaa5919119b3b35ff5af83a7fc8989d9d29
fabferreira <39332665+fabferreira@users.noreply.github.com>
2018-07-10 01:20:46 +0100

    Update challenge2.specs.js

--- test/specs/challenge2.specs.js
+++ test/specs/challenge2.specs.js
@@ -9,6 +9,8 @@ describe('Mindera QA Graduate Challenge 2:', function () {
 
     it('Shoot all the bubbles that showed up after waiting 5 seconds', function () {
         browser.pause(5000);
-        /** Insert the code here */
+        var count = challenge2Page.bulle.length;
+        challenge2Page.bulle.click()
+        expect(challenge2Page.score).to.be.equal(count)
     });
 });
